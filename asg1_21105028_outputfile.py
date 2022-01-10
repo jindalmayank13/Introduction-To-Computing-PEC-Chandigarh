9b003bf52ca674e3f63e004169f48e0c6eb6fcbf
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2022-01-10 19:31:12 +0530

    Add files via upload

---
+++ asg1_21105028_outputfile.py
@@ -0,0 +1,38 @@
+### OUTPUT OF THE CODE ###
+### Q1 ###
+Enter first number -30
+Enter second number -40
+Enter third numer -50
+Average -40.0
+
+### Q2 ###
+
+Enter the Gross Income to nearest penny -4500000
+Enter the number of Dependents -4
+895600.0
+
+### Q3 ###
+
+Enter your SID-21105028
+Enter your name-Mayank Jindal
+Enter your Gender-(M,F,U) - M
+Enter the course name - ECE
+Enter the CGPA -10
+[21105028, 'Mayank Jindal','M','ECE',10.0]
+
+### Q4 ###
+
+Enter the marks in Subject 1 = 98
+Enter the marks in Subject 2 = 97
+Enter the marks in Subject 3 = 100
+Enter the marks in Subject 4 = 98
+Enter the marks in Subject 5 = 99
+[97.0,98.0,98.0,99.0,100.0]
+
+### Q5(a) ###
+
+['Red', 'Green','White','Pink','Yellow']
+
+### Q5(b) ###
+
+['Red', 'Green','White','Purple','Yellow']
